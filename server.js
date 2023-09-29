ad2d4e43935db156450652cbd1bcda36a2da1419
Replit user <>
2023-09-29 13:14:49 +0000

    third commit

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/express');
 var app = express();
+const keepAlive = require('./server');
+const Monitor = require('ping-monitor');
 app.listen(3000);
 module.exports = app;
 console.log('Server running at http://localhost:3000/');
+keepAlive();
+const monitor = new Monitor({
+    website: '',
+    title: 'NAME',
+    interval: 2
+});
