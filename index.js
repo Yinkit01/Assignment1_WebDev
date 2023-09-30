a85722242fd3c53f8f2f6a80b744c35fb622825b
Replit user <>
2023-09-30 18:25:40 +0000

    home/about/navBar/footer commit

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var app = express();
 app.listen(3000);
 module.exports = app;
 console.log('Server running at http://localhost:3000/');
-// keepAlive();
-// const keepAlive = require('./server');
-// const Monitor = require('ping-monitor');
-// const monitor = new Monitor({
-//   website: '',
-//   title: 'NAME',
-//   interval: 2
-// });
+keepAlive();
+const keepAlive = require('./server');
+const Monitor = require('ping-monitor');
+const monitor = new Monitor({
+  website: '',
+  title: 'NAME',
+  interval: 2
+});
